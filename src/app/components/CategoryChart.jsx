dd5f65045d15970b2f2f2c9b13a415eb761f98ac
Samantha Brennan <samanthabrennan@usf.edu>
2025-12-07 17:19:06 -0500

    Added product filter to and length to Category Chart

--- src/app/components/CategoryChart.jsx
+++ src/app/components/CategoryChart.jsx
@@ -1,9 +1,24 @@
 "use client";
-export default function CategoryChart() {
+export default function CategoryChart({ products }) {
+  if (!products) {
+    return null;
+  }
+
   return (
     <div className="category-chart">
       <h2>Products by Category</h2>
-      <p> This will show a simple bar chart based on the current products.</p>
+      <p>
+        {" "}
+        View {products.length} product {products.length !== 1 ? "s" : ""}.
+      </p>
+
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name} - Category: {p.category}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
