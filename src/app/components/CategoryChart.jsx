31c97c803c833fae1d2f5abb17877fc24fd86b1c
Samantha Brennan <samanthabrennan@usf.edu>
2025-12-07 17:05:55 -0500

    Created Category Chart file in components

---
+++ src/app/components/CategoryChart.jsx
@@ -0,0 +1,9 @@
+"use client";
+{
+  return (
+    <div className="category-chart">
+      <h2>Products by Category</h2>
+      <p> This will show a simple bar chart based on the current products.</p>
+    </div>
+  );
+}
